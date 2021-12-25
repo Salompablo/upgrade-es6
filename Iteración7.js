65b1985c01183bf878db7eff866453eccc463989
Salompablo <pablosalompita@gmail.com>
2021-12-25 17:29:41 +0100

    Create Iteración7

---
+++ Iteración7.js
@@ -0,0 +1,27 @@
+const exams = [
+    {name: 'Yuyu Cabeza Crack', score: 5}, 
+    {name: 'Maria Aranda Jimenez', score: 1}, 
+    {name: 'Cristóbal Martínez Lorenzo', score: 6}, 
+    {name: 'Mercedez Regrera Brito', score: 7},
+    {name: 'Pamela Anderson', score: 3},
+    {name: 'Enrique Perez Lijó', score: 6},
+    {name: 'Pedro Benitez Pacheco', score: 8},
+    {name: 'Ayumi Hamasaki', score: 4},
+    {name: 'Robert Kiyosaki', score: 2},
+    {name: 'Keanu Reeves', score: 10}
+];
+
+let totalScore = exams.reduce((acc, item) => acc + item.score, 0)
+
+let totalAprobados = exams.reduce((acc, item) => {
+    if (item.score >= 5) {
+        return acc + item.score
+    } else {
+        return acc
+    }
+} ,0)
+
+let media = totalScore / exams.length
+
+
+
